b3d97009bee165a9f644b317a1061ad7b16888ff
8024 <chenming166613@outlook.com>
2017-10-31 19:42:24 +0800

    project3-cpp-new
    new code for cpp_project3

---
+++ U201514496_3_NEW/U201514496_3/U201514496_3/STACK.h
@@ -0,0 +1,19 @@
+#include<stdio.h>
+class STACK {
+public:
+	STACK(int m);
+	STACK(const STACK& s);
+	virtual int size() const;
+	virtual operator int() const;
+	virtual int operator[] (int x) const;
+	virtual STACK& operator<<(int e);
+	virtual STACK& operator >> (int &e);
+	virtual STACK& operator=(const STACK& s);
+	virtual void print(FILE *fp) const;
+	virtual ~STACK();
+	bool m_bError_exist;
+private:
+	int *const m_iElems;
+	const int m_iMax;
+	int m_iPos;
+};
